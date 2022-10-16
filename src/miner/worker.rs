d2f4bf871ba2a5b4695b6fad642c3042b2b53316
nancyx24 <113613979+nancyx24@users.noreply.github.com>
2022-10-15 23:14:36 -0400

    Update worker.rs

--- src/miner/worker.rs
+++ src/miner/worker.rs
@@ -3,21 +3,26 @@ use log::{debug, info};
 use crate::types::block::Block;
 use crate::network::server::Handle as ServerHandle;
 use std::thread;
+use crate::blockchain::Blockchain;
+use std::sync::{Arc, Mutex};
 
 #[derive(Clone)]
 pub struct Worker {
     server: ServerHandle,
     finished_block_chan: Receiver<Block>,
+    blockchain: Arc<Mutex<Blockchain>>,
 }
 
 impl Worker {
     pub fn new(
         server: &ServerHandle,
         finished_block_chan: Receiver<Block>,
+        blockchain: Arc<Mutex<Blockchain>>,
     ) -> Self {
         Self {
             server: server.clone(),
             finished_block_chan,
+            blockchain,
         }
     }
 
@@ -35,6 +40,8 @@ impl Worker {
         loop {
             let _block = self.finished_block_chan.recv().expect("Receive finished block error");
             // TODO for student: insert this finished block to blockchain, and broadcast this block hash
+            let mut blockchain = self.blockchain.lock().unwrap();
+            blockchain.insert(&_block);
         }
     }
 }
