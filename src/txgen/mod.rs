1da3bb1aaa851e88eaec74411f5aaad433cc591d
nancyx24 <113613979+nancyx24@users.noreply.github.com>
2022-11-26 15:40:22 -0500

    Update mod.rs

--- src/txgen/mod.rs
+++ src/txgen/mod.rs
@@ -11,10 +11,13 @@ use std::thread;
 use std::sync::{Arc, Mutex};
 
 use crate::blockchain::Mempool;
+use crate::blockchain::Blockchain;
 use crate::types::address::Address;
 use super::types::hash::Hashable;
 use super::types::transaction::{Transaction, SignedTransaction};
 use crate::types::key_pair;
+
+use rand::Rng;
 
 enum ControlSignal {
     Start(u64), // the number controls the theta of interval between block generation
@@ -33,7 +36,9 @@ pub struct Context {
     control_chan: Receiver<ControlSignal>,
     operating_state: OperatingState,
     finished_transaction_chan: Sender<SignedTransaction>,
+    blockchain: Arc<Mutex<Blockchain>>,
     mempool: Arc<Mutex<Mempool>>,
+    accounts: Vec<Ed25519KeyPair>, // key is key_pair, value is (value, nonce)
 }
 
 #[derive(Clone)]
@@ -58,17 +63,22 @@ impl Handle {
     }
 }
 
-pub fn new(mempool: Arc<Mutex<Mempool>>) -> (Context, Handle, Receiver<SignedTransaction>) {
+pub fn new(mempool: Arc<Mutex<Mempool>>, key_pair: Ed25519KeyPair, blockchain: Arc<Mutex<Blockchain>>) -> (Context, Handle, Receiver<SignedTransaction>) {
     let (signal_chan_sender, signal_chan_receiver) = unbounded();
     let (finished_tx_sender, finished_tx_receiver) = unbounded();
 
     let clone_mempool = Arc::clone(&mempool);
 
+    let mut accounts: Vec<Ed25519KeyPair> = Vec::new();
+    accounts.push(key_pair);
+
     let ctx = Context {
         control_chan: signal_chan_receiver,
         operating_state: OperatingState::Paused,
         finished_transaction_chan: finished_tx_sender,
+        blockchain,
         mempool: clone_mempool,
+        accounts,
     };
 
     let handle = Handle {
@@ -138,22 +148,73 @@ impl Context {
                 return;
             }
 
-            // generate signed transaction
-            let receiver = Address::from([0u8; 20]);
-            let value = rand::random();
-            let nonce = rand::random();
-            let key_pair_rand = key_pair::random();
-            let public_key = key_pair_rand.public_key().as_ref().to_vec();
+            // generate random receiver address
+            let mut prob_rng = rand::thread_rng();
+            let prob = prob_rng.gen_range(0.0..1.0);
 
-            let transaction = Transaction::new(receiver, value, nonce);
-            let sig = transaction::sign(&transaction, &key_pair_rand).as_ref().to_vec();
-            let signed_transaction = SignedTransaction::new(transaction, sig, public_key);
+            if prob < 0.1 {
+                let prob_receiver_key_pair = key_pair::random();
+                self.accounts.push(prob_receiver_key_pair); // ASK ABOUT THIS
+            }
+
+            // generate signed transaction
+
+            // FOR SENDER
+            for sender in &self.accounts {
+                println!("self.accounts not empty");
+                let tip_hash = {self.blockchain.lock().unwrap().tip()};
+                let tip_block = {self.blockchain.lock().unwrap().get_parent_block(tip_hash)};
+                let state = tip_block.get_state();
+                println!("state empty?");
+                println!("{}", state.get_state().is_empty());
+
+                let sender_address = Address::from_public_key_bytes(sender.public_key().as_ref());
+
+                if state.contains_key(sender_address) {
+                    println!("state contains key");
+                    let mut rng = rand::thread_rng();
+                
+                    let sender_nonce = state.get(sender_address.clone()).0;
+                    let sender_balance = state.get(sender_address.clone()).1;
+
+                    // set value and nonce of signed transaction
+                    let value: u32;
+
+                    if sender_balance > 0 {
+                        if sender_balance > 10 {
+                            value = rng.gen_range(1..10);
+                        }
+                        else {
+                            value = rng.gen_range(1..sender_balance);
+                        }
+                    }
+                    else {
+                        value = 0;
+                    }
+                    
+                    let nonce = sender_nonce + 1;
+
+                    // FOR RECEIVER
+
+                    // generate receiver address
+                    // if prob is less than 0.1, create another account
+                    let receiver_index = rng.gen_range(0..self.accounts.len());
+                    let receiver_key_pair = self.accounts.get(receiver_index).unwrap();
+                    let receiver = Address::from_public_key_bytes(receiver_key_pair.public_key().as_ref());
+
+                    let transaction = Transaction::new(receiver, value, nonce);
+                    let sig = transaction::sign(&transaction, &sender).as_ref().to_vec();
+                    let signed_transaction = SignedTransaction::new(transaction, sig, sender.public_key().as_ref().to_vec());
+                    
+                    println!("transaction generated in txgen mod");
             
-            self.finished_transaction_chan.send(signed_transaction.clone()).expect("Send finished block error");
-            println!("{}", nonce);
-            // put into mempool
-            {self.mempool.lock().unwrap().insert(signed_transaction.hash(), &signed_transaction)};
-            println!("after insert into mempool");
+                    self.finished_transaction_chan.send(signed_transaction.clone()).expect("Send finished block error");
+                    // put into mempool
+                    {self.mempool.lock().unwrap().insert(signed_transaction.hash(), &signed_transaction)};
+                }
+                
+                
+            }
 
             // END OF MY CODE
             if let OperatingState::Run(i) = self.operating_state {
