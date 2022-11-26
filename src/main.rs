4ef42a4af157b7568933e08d539abcee74028e3a
nancyx24 <113613979+nancyx24@users.noreply.github.com>
2022-11-26 15:42:50 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ use std::process;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time;
+use ring::signature::Ed25519KeyPair;
 
 fn main() {
     // parse command line arguments
@@ -36,8 +37,7 @@ fn main() {
     // init logger
     let verbosity = matches.occurrences_of("verbose") as usize;
     stderrlog::new().verbosity(verbosity).init().unwrap();
-    let blockchain = Arc::new(Mutex::new(Blockchain::new()));
-    let mempool = Arc::new(Mutex::new(Mempool::new()));
+    
     // parse p2p server address
     let p2p_addr = matches
         .value_of("peer_addr")
@@ -47,6 +47,22 @@ fn main() {
             error!("Error parsing P2P server address: {}", e);
             process::exit(1);
         });
+
+    // generate keys for each node
+    let mut key_pair = Ed25519KeyPair::from_seed_unchecked(&[0; 32]).unwrap(); // seed is 0 for 6000
+    let mut key_pair_clone = Ed25519KeyPair::from_seed_unchecked(&[0; 32]).unwrap();
+
+    if p2p_addr == "127.0.0.1:6001".parse::<net::SocketAddr>().unwrap() {
+        key_pair = Ed25519KeyPair::from_seed_unchecked(&[1; 32]).unwrap(); // seed is 1 for 6001
+        key_pair_clone = Ed25519KeyPair::from_seed_unchecked(&[1; 32]).unwrap();
+    }
+    else if p2p_addr == "127.0.0.1:6002".parse::<net::SocketAddr>().unwrap() {
+        key_pair = Ed25519KeyPair::from_seed_unchecked(&[2; 32]).unwrap(); // seed is 2 for 6002
+        key_pair_clone = Ed25519KeyPair::from_seed_unchecked(&[1; 32]).unwrap();
+    }
+
+    let blockchain = Arc::new(Mutex::new(Blockchain::new(key_pair)));
+    let mempool = Arc::new(Mutex::new(Mempool::new()));
 
     // parse api server address
     let api_addr = matches
@@ -84,7 +100,7 @@ fn main() {
     worker_ctx.start();
 
     // start the transaction generator
-    let (tx_ctx, tx, finished_block_chan_tx) = txgen::new(mempool.clone());
+    let (tx_ctx, tx, finished_block_chan_tx) = txgen::new(mempool.clone(), key_pair_clone, blockchain.clone());
     let tx_worker_ctx = txgen::worker::Worker::new(&server, finished_block_chan_tx, mempool.clone());
     tx_ctx.start();
     tx_worker_ctx.start();
